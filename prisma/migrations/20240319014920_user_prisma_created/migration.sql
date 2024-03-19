5eda60f4feb7ace9b96c67131539ad11151bc285
Gabriel Lima <gabriel58221@gmail.com>
2024-03-18 22:51:40 -0300

    prisa user created

---
+++ prisma/migrations/20240319014920_user_prisma_created/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'DELIVERY_MAN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'DELIVERY_MAN',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_cpf_key" ON "users"("cpf");
